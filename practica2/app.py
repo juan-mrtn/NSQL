45e1e224e7d381fe9aca1765a8222a6e351a4e57
Juan Acuña <juan.acuna.jma@gmail.com>
2026-04-08 19:26:16 -0300

    details

--- practica2/app.py
+++ practica2/app.py
@@ -12,26 +12,26 @@ def inicializar_db():
     # Temporada 1
     if r.llen("temporada:1") == 0:
         r.lpush("temporada:1", 
-                "Capítulo 8: Redención", "Capítulo 7: El ajuste de cuentas", 
-                "Capítulo 6: El prisionero", "Capítulo 5: El pistolero", 
-                "Capítulo 4: Santuario", "Capítulo 3: El pecado", 
-                "Capítulo 2: El niño", "Capítulo 1: El mandaloriano")
+                "Capitulo 8: Redencion", "Capitulo 7: El ajuste de cuentas", 
+                "Capitulo 6: El prisionero", "Capitulo 5: El pistolero", 
+                "Capitulo 4: Santuario", "Capitulo 3: El pecado", 
+                "Capitulo 2: El niño", "Capitulo 1: El mandaloriano")
 
     # Temporada 2
     if r.llen("temporada:2") == 0:
         r.lpush("temporada:2", 
-                "Capítulo 16: El rescate", "Capítulo 15: El creyente", 
-                "Capítulo 14: La tragedia", "Capítulo 13: La Jedi", 
-                "Capítulo 12: El asedio", "Capítulo 11: La heredera", 
-                "Capítulo 10: La pasajera", "Capítulo 9: El mariscal")
+                "Capitulo 16: El rescate", "Capitulo 15: El creyente", 
+                "Capitulo 14: La tragedia", "Capitulo 13: La Jedi", 
+                "Capitulo 12: El asedio", "Capitulo 11: La heredera", 
+                "Capitulo 10: La pasajera", "Capitulo 9: El mariscal")
 
     # Temporada 3
     if r.llen("temporada:3") == 0:
         r.lpush("temporada:3", 
-                "Capítulo 24: El regreso", "Capítulo 23: Los espías", 
-                "Capítulo 22: Pistoleros a sueldo", "Capítulo 21: El pirata", 
-                "Capítulo 20: El huérfano", "Capítulo 19: El converso", 
-                "Capítulo 18: Las minas de Mandalore", "Capítulo 17: El apóstata")
+                "Capitulo 24: El regreso", "Capitulo 23: Los espias", 
+                "Capitulo 22: Pistoleros a sueldo", "Capitulo 21: El pirata", 
+                "Capitulo 20: El huerfano", "Capitulo 19: El converso", 
+                "Capitulo 18: Las minas de Mandalore", "Capitulo 17: El apostata")
 
 @app.route('/')
 def index():
@@ -43,12 +43,10 @@ def index():
     }
     
     for nombre, redis_key in temporadas.items():
-        # Obtenemos la lista completa con LRANGE 
         titulos = r.lrange(redis_key, 0, -1)
         catalogo_visual[nombre] = []
 
         for titulo in titulos:
-            # Consultamos el estado temporal (SETEX)
             estado = r.get(f"estado:{titulo}") or "Disponible"
             catalogo_visual[nombre].append({
                 "titulo": titulo,
@@ -60,7 +58,6 @@ def index():
 @app.route('/reservar', methods=['POST'])
 def reservar():
     titulo = request.form.get('titulo')
-    # 2. Reserva por 4 minutos (240 segundos) si no está ocupado [cite: 65]
     if not r.exists(f"estado:{titulo}"):
         r.setex(f"estado:{titulo}", 240, "Reservado (Pendiente de Pago)")
         flash(f"'{titulo}' reservado por 4 min.", "warning")
@@ -72,7 +69,6 @@ def reservar():
 def pagar():
     titulo = request.form.get('titulo')
     precio = request.form.get('precio')
-    # 3. Confirmar pago y registrar alquiler por 24 hs (86400 segundos) [cite: 66]
     r.setex(f"mando:estado:{titulo}", 86400, "Alquilado")
     flash(f"Pago de ${precio} recibido. '{titulo}' alquilado por 24hs.", "success")
     return redirect(url_for('index'))
